2c70b5f40b5e35bab09dc5c4015ec079182531eb
ismael-santos <ismael.tostring@gmail.com>
2019-09-13 13:14:39 -0300

    teste ilegra

---
+++ Business/Mapper/MapperItem.cs
@@ -0,0 +1,39 @@
+﻿using Interface.Mapper;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mapper
+{
+    public class MapperItem : IMapper<string, List<Item>>
+    {
+        private MapperItem() { }
+
+        public static MapperItem Instance => new MapperItem();
+
+        public List<Item> Map(string @in)
+        {
+            List<Item> result = new List<Item>();
+
+            string itemData = @in.Replace("[", string.Empty)
+                                 .Replace("]", string.Empty);
+
+            List<string> allItemList = itemData.Split(',').ToList();
+
+            allItemList.ForEach(item => 
+            {
+                List<string> itemDetail = item.Split('-').ToList();
+
+                result.Add(new Item
+                {
+                    ID = itemDetail[0],
+                    Quantity = Convert.ToInt32(itemDetail[1]),
+                    Price = Convert.ToDecimal(itemDetail[2].Replace(".", ",")) //Considerar as casas decimais
+                });
+            });
+
+            return result;
+        }
+    }
+}
---
+++ Business/Mapper/MapperSalesman.cs
@@ -0,0 +1,28 @@
+﻿using Interface.Mapper;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mapper
+{
+    public class MapperSalesman : IMapper<string, Salesman>
+    {
+        private MapperSalesman() { }
+
+        public static MapperSalesman Instance => new MapperSalesman();
+
+        public Salesman Map(string @in)
+        {
+            List<string> salesmanData = @in.Split('ç').ToList();
+
+            return new Salesman
+            {
+                ID = salesmanData[0],
+                CPF = salesmanData[1],
+                Name = salesmanData[2],
+                Salary = Convert.ToDecimal(salesmanData[3].Replace(".", ",")) //Considerar as casas decimais
+            };
+        }
+    }
+}
